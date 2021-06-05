7be1a2d282cfff0584989a8c8f17e1bcaa64c1cc
Tanvee Bajaj <81170127+tanveebajaj36@users.noreply.github.com>
2021-06-05 16:22:36 +0530

    Add files via upload

---
+++ passwordauthentication.py
@@ -0,0 +1,12 @@
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+print("Your username and password is saved.")
+
+username1 = input("Enter your username: ")
+password1 = input("Enter your password: ")
+if username == username1 and password == password1:
+    print("Verified")
+elif username != username1 or password != password1:
+    print("Invalid Username or password")
+    username1 = input("Enter your username: ")
+    password1 = input("Enter your password: ")
